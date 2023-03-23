98b7b4f13a3ae37ed09177978e79260cc7ec2215
joshjoshcha <128344150+joshjoshcha@users.noreply.github.com>
2023-03-22 23:32:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# asdfaklsdf fuck o ujosh cha
+# josh cha
+
+def password_encoder(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+  password = "12345555"
+encoded_password = password_encoder(password)
+print(encoded_password)  # prints "45678888"
+
+password = "00009962"
+encoded_password = password_encoder(password)
+print(encoded_password)  # prints "33332295"
