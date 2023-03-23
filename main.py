0b23aafd8e54ed363b59f85b634f1f2e9c355462
joshjoshcha <128344150+joshjoshcha@users.noreply.github.com>
2023-03-22 23:38:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ def password_encoder(password):
         encoded_password += encoded_digit
     return encoded_password
 
-  password = "12345555"
-encoded_password = password_encoder(password)
-print(encoded_password)  # prints "45678888"
-
-password = "00009962"
-encoded_password = password_encoder(password)
-print(encoded_password)  # prints "33332295"
+while True:
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+    option = input("Please enter an option: ")
+    if option == "1":
+        password = input("Please enter your password to encode: ")
+        encoded_password = password_encoder(password)
+        print("Your password has been encoded and stored!")
+    elif option == "2":
+        if "encoded_password" not in locals():
+            print("No encoded password has been stored yet.")
+            continue
+        decoded_password = password_decoder(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+    elif option == "3":
+        break
+    else:
+        print("Invalid option. Please try again.")
